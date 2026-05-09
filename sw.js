d6f61c9f137327b5153271105a67603d0f2ba556
tj065k6 <tj065k6@gmail.com>
2026-05-10 02:55:35 +0800

    Initial lens thickness calculator PWA

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE = 'lens-v1';
+const BASE = '/lens-calculator';
+const FILES = [BASE+'/', BASE+'/index.html', BASE+'/manifest.json', BASE+'/icon-192.png', BASE+'/icon-512.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
